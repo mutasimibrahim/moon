792c06c98a1c490ca8aa24f2dbabeb644ba28c2f
Mutasim Ibrahim <mutasimibrahim01@gmail.com>
2024-03-09 11:13:35 -0600

    Learned javascript fetch api

--- file1.js
+++ file1.js
@@ -1,5 +1,31 @@
-hello
-world
-world
-test
-sky
+fetch("https://www.boredapi.com/api/activity/") // Target the correct endpoint
+
+.then(res => {
+    console.log(res.ok, res.status)
+    return res.json();
+})
+
+.then(data => {
+    console.log(`You could ${data.activity}`)
+})
+
+.catch(err => {
+    console.log('Failed to connect:', err.message)
+})
+
+/*
+.then(response => {
+if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+}
+return response.json();
+})
+
+.then(data => {
+console.log(`You could ${data.activity}`); // Access the suggested activity
+})
+
+.catch(error => {
+console.error("Error:", error); // Handle any errors
+});
+*/
