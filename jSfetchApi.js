fdcf5e8d1d21edc8c36ea41b6e4f2a6db0d5db2b
Mutasim Ibrahim <mutasimibrahim01@gmail.com>
2024-03-09 11:18:23 -0600

    added .then block

--- jSfetchApi.js
+++ jSfetchApi.js
@@ -1,31 +1,18 @@
 fetch("https://www.boredapi.com/api/activity/") // Target the correct endpoint
 
 .then(res => {
-    console.log(res.ok, res.status)
+    console.log(res.ok, res.status);
     return res.json();
 })
 
 .then(data => {
-    console.log(`You could ${data.activity}`)
-})
-
-.catch(err => {
-    console.log('Failed to connect:', err.message)
-})
-
-/*
-.then(response => {
-if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-}
-return response.json();
+    console.log(`You could ${data.activity}`);
 })
 
 .then(data => {
-console.log(`You could ${data.activity}`); // Access the suggested activity
+    console.log(data);
 })
 
-.catch(error => {
-console.error("Error:", error); // Handle any errors
-});
-*/
+.catch(err => {
+    console.log('Failed to connect:', err.message);
+})
